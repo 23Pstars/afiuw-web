82ef7afe02fcdd3b2939936f9581c7f9c8ee1e99
Ahmad Zafrullah <23Pstars@gmail.com>
2019-09-14 12:19:32 +0800

    update gulp v4

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,21 @@ const gulp = require('gulp');
 const htmlclean = require('gulp-htmlclean');
 const cssnano = require('gulp-cssnano');
 
-gulp.task('html', () => {
+gulp.task('html', done => {
     gulp.src('./index.html')
         .pipe(htmlclean())
-        .pipe(gulp.dest('_build'))
+        .pipe(gulp.dest('_build'));
+    done();
 });
 
-gulp.task('css', () => {
+gulp.task('css', done => {
     gulp.src(['./assets/css/ie8.css', './assets/css/ie9.css', './assets/css/main.css', './assets/css/noscript.css'])
         .pipe(cssnano())
         .pipe(gulp.dest('_build/assets/css'));
+    done();
 });
 
-gulp.task('copy', () => {
+gulp.task('copy', done => {
     gulp.src('./assets/js/*.js')
         .pipe(gulp.dest('_build/assets/js'));
     gulp.src('./assets/css/font-awesome.min.css')
@@ -27,6 +29,7 @@ gulp.task('copy', () => {
         .pipe(gulp.dest('_build/images'));
     gulp.src('./.htaccess')
         .pipe(gulp.dest('_build'));
+    done();
 });
 
-gulp.task('default', ['html', 'css', 'copy']);
+gulp.task('default', gulp.parallel(['html', 'css', 'copy']));
